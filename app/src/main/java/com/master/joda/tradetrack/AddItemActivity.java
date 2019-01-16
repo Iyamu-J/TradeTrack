9e6dbd4743c5cb18da4f4819cb4dea6250d221ed
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-16 09:42:46 +0100

    Bind views using ButterKnife
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/com/master/joda/tradetrack/AddItemActivity.java
+++ app/src/main/java/com/master/joda/tradetrack/AddItemActivity.java
@@ -1,13 +1,30 @@
 package com.master.joda.tradetrack;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    @BindView(R.id.edit_item_name)
+    EditText editItemName;
+    @BindView(R.id.edit_cost_price)
+    EditText editCostPrice;
+    @BindView(R.id.edit_selling_price)
+    EditText editSellingPrice;
+    @BindView(R.id.edit_quantity)
+    EditText editQuantity;
+    @BindView(R.id.fab_add_item)
+    FloatingActionButton fabAddItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+        ButterKnife.bind(this);
     }
 }
