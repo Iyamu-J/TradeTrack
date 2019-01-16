1790a8404d9631e828a53ee47ca03b834595d4d9
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-16 11:36:48 +0100

    Initialise Firebase Database
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/com/master/joda/tradetrack/HomeFragment.java
+++ app/src/main/java/com/master/joda/tradetrack/HomeFragment.java
@@ -8,15 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
  */
 public class HomeFragment extends Fragment {
 
-    private OnListFragmentInteractionListener mListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mItemsDatabaseReference;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -37,38 +38,10 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_item_list, container, false);
 
         Context context = view.getContext();
+
+        // initialise FirebaseDatabase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mItemsDatabaseReference = mFirebaseDatabase.getReference();
         return view;
-    }
-
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnListFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
     }
 }
