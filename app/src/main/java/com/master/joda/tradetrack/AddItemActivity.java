2652a7d1312fc59256c685b50c125a038e023847
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-17 09:54:01 +0100

    Create method to generate random ID
    A random ID is used to uniquely identify each item.
    The Id of the FirebaseUser is needed to separate client data
    
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/com/master/joda/tradetrack/AddItemActivity.java
+++ app/src/main/java/com/master/joda/tradetrack/AddItemActivity.java
@@ -1,5 +1,6 @@
 package com.master.joda.tradetrack;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,9 +8,12 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.master.joda.tradetrack.model.Item;
+
+import java.util.UUID;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +31,7 @@ public class AddItemActivity extends AppCompatActivity {
     @BindView(R.id.fab_add_item)
     FloatingActionButton fabAddItem;
 
+    private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mDatabaseReference;
 
     @Override
@@ -35,21 +40,33 @@ public class AddItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_item);
         ButterKnife.bind(this);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("items");
 
         fabAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String id = generateId();
                 Item item = new Item(
-                        1,
+                        id,
                         editItemName.getText().toString(),
                         editQuantity.getText().toString(),
                         Double.valueOf(editSellingPrice.getText().toString()),
                         Double.valueOf(editCostPrice.getText().toString())
                 );
-                mDatabaseReference.child("items").setValue(item);
-                finish();
+                FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                String userId = null;
+                if (user != null) {
+                    userId = user.getUid();
+                    mDatabaseReference.child(userId).child(id).setValue(item);
+                    finish();
+                }
             }
         });
+    }
+
+    @NonNull
+    private String generateId() {
+        return UUID.randomUUID().toString();
     }
 }
