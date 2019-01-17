7c924eb57c2f0345f1d6ca618f6e5799ac875dda
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-17 09:19:42 +0100

    Implement adding an Item to the database
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/com/master/joda/tradetrack/AddItemActivity.java
+++ app/src/main/java/com/master/joda/tradetrack/AddItemActivity.java
@@ -3,7 +3,13 @@ package com.master.joda.tradetrack;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.master.joda.tradetrack.model.Item;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,10 +27,29 @@ public class AddItemActivity extends AppCompatActivity {
     @BindView(R.id.fab_add_item)
     FloatingActionButton fabAddItem;
 
+    private DatabaseReference mDatabaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
         ButterKnife.bind(this);
+
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("items");
+
+        fabAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Item item = new Item(
+                        1,
+                        editItemName.getText().toString(),
+                        editQuantity.getText().toString(),
+                        Double.valueOf(editSellingPrice.getText().toString()),
+                        Double.valueOf(editCostPrice.getText().toString())
+                );
+                mDatabaseReference.child("items").setValue(item);
+                finish();
+            }
+        });
     }
 }
