f3af0115f6cc2064148f3df04fcef7d681652986
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-24 07:11:10 +0100

    Initialise Firebase DatabaseReference
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

--- app/src/main/java/com/master/joda/tradetrack/ViewRecordsFragment.java
+++ app/src/main/java/com/master/joda/tradetrack/ViewRecordsFragment.java
@@ -7,11 +7,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
+import butterknife.ButterKnife;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ViewRecordsFragment extends Fragment {
 
+    private Date date;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mRecordsDatabaseReference;
 
     public ViewRecordsFragment() {
         // Required empty public constructor
@@ -22,7 +34,12 @@ public class ViewRecordsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_records, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_records, container, false);
+
+        mRecordsDatabaseReference = FirebaseDatabase.getInstance().getReference().child("records");
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        return view;
     }
 
 }
