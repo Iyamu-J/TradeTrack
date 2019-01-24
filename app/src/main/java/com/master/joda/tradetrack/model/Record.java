639af999167dea1e576d7117b08380d01e79061e
Iyamu Joshua <ayevbeosa.j@gmail.com>
2019-01-24 13:49:55 +0100

    Initial Commit
    Signed-off-by: Iyamu Joshua <ayevbeosa.j@gmail.com>

---
+++ app/src/main/java/com/master/joda/tradetrack/model/Record.java
@@ -0,0 +1,25 @@
+package com.master.joda.tradetrack.model;
+
+public class Record {
+
+    private String date;
+    private String profit;
+
+    public Record() {}
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getProfit() {
+        return profit;
+    }
+
+    public void setProfit(String profit) {
+        this.profit = profit;
+    }
+}
